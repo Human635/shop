60539aa428f40160db6865a32376c9d2b76995d9
Xojiakbar <139034828+Human635@users.noreply.github.com>
2023-07-27 21:37:49 +0500

    Delete_btn

--- fe-17-inventory-start-js/index.js
+++ fe-17-inventory-start-js/index.js
@@ -1,37 +1,59 @@
+//инпуты
 let productType = document.querySelector('#type-select')
 let productName = document.querySelector('#product-name')
 let productCount = document.querySelector('#product-count')
 
+//кнопки
 let addProductButton = document.querySelector('.add-product')
 let productsListElement = document.querySelector('.products-list')
 
 let productsArray = []
+let countId = 0
 
-addProductButton.addEventListener('click', function(event) {
+addProductButton.addEventListener('click', addProduct)
+
+function addProduct() {
   let productTypeValue = productType.value 
   let productNameValue = productName.value 
   let productCountValue = productCount.value
+
+  countId += 1
+
+  let productId = countId
   
   let productObj = {
+    productId: productId,
     productTypeKey: productTypeValue,
     productNameKey: productNameValue,
     productCountKey: productCountValue
   }
-  
-  productsArray.push(productObj)
 
-  console.log(productsArray)  
+  productsArray.push(productObj)
+  rerederProductsTable()
+}
+  
+function deleteProduct(productId) {
+  //Через фильтр
+  //productsArray.filter(product => product.productId !== productId)
+  //Через slice
+  let productIdx = productsArray.findIndex(product => product.productId === productId)
+  productsArray.splice(productIdx, 1)
+  rerederProductsTable()
+}
+
+function rerederProductsTable(){
   let innerHtmlWithProducts = ''
   
   productsArray.forEach((item) => {
     innerHtmlWithProducts += `<div class='product'>
     <div class='description-wrapper'>
-      <p>Тип: ${item.productTypeKey}</p>
+    <p>Тип: ${item.productTypeKey}</p>
       <p>Название: ${item.productNameKey}</p>
       <p>Количество: ${item.productCountKey}</p>
+      <button class="remove-button" onclick="deleteProduct(${item.productId})">Delete</button>
     </div>
   </div>`
   })
   
   productsListElement.innerHTML = innerHtmlWithProducts
-  })
+}
